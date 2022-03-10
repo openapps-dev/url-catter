1dbab079c6067e2e81a5a5192cc572dc16fcd93f
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-03-10 22:21:18 +0100

    feat(Application): change `commandLineRunner`

--- src/main/java/com/produo/urlcatter/Application.java
+++ src/main/java/com/produo/urlcatter/Application.java
@@ -11,22 +11,14 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("> Server started...");
+        };
+    }
 }

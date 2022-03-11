159999f2cb42ca8ca9ebaf2a49192a7c0377bde3
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-03-11 08:50:51 +0100

    feat(link): `/{code}` redirect instead of returning url

--- src/main/java/com/produo/urlcatter/link/LinkController.java
+++ src/main/java/com/produo/urlcatter/link/LinkController.java
@@ -3,6 +3,7 @@ package com.produo.urlcatter.link;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.HashMap;
 
@@ -20,10 +21,10 @@ public class LinkController {
     }
 
     @GetMapping("/{code}")
-    public ResponseEntity<String> getLink(@PathVariable("code") String code) {
+    public Object getLink(@PathVariable("code") String code) {
         LinkEntity link = repository.findByCode(code);
         if (link != null)
-            return ResponseEntity.status(HttpStatus.OK).body(link.getOriginal());
+            return new RedirectView(link.getOriginal());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("no link");
     }
 

e4daaa6bfff306f8f64b524ab7c9f1740bf45c83
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-03-11 13:23:13 +0100

    feat(LinkRepository): add findByOriginal

--- src/main/java/com/produo/urlcatter/link/LinkRepository.java
+++ src/main/java/com/produo/urlcatter/link/LinkRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LinkRepository extends JpaRepository<LinkEntity, Integer> {
     LinkEntity findByCode(String code);
+
+    LinkEntity findByOriginal(String original);
 }
